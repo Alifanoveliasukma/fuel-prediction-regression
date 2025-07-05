8bf0e38c5f80d5975ee4ad3c3a2ce2b94234e324
Alifanoveliasukma <alifasukma11@gmail.com>
2025-07-05 21:35:04 +0700

    feat: init fuel prediction API

---
+++ regression_api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return "Fuel Prediction Regression API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    liter = data.get('liter')
+
+    if liter is None:
+        return jsonify({'error': 'Field \"liter\" is required'}), 400
+
+    try:
+        prediction = model.predict([[float(liter)]])
+        return jsonify({
+            'liter': liter,
+            'prediksi_kilometer': round(float(prediction[0][0]), 2)
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
